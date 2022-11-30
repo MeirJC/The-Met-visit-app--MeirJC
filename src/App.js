6585a75d9af52899ff5e83d0c7db20f156128b13
Meir J Cohen <meir84@gmail.com>
2022-11-30 03:23:18 +0200

    added navbar and dinamicly add art items cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ArtItemsList from "./components/ArtItemsList/ArtItemsList";
-
-
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar />
+      <ArtItemsList />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ArtItemsList/>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/components/ArtItemsList/ArtItemsList.jsx
+++ src/components/ArtItemsList/ArtItemsList.jsx
@@ -1,51 +1,130 @@
-import React from 'react'
-import {useState} from "react";
+import React from "react";
+import { useState } from "react";
 import axios from "axios";
-
+import "./ArtItemsList.css";
 //================================================
 function ArtItemsList() {
   const [categories, setCategories] = useState([]);
   const [artItemsIds, setArtItemsIds] = useState([]);
-  // const [artItems, setArtItems] = useState([]);
+  const [artItems, setArtItems] = useState([]);
+  const [counter, setCounter] = useState(0);
   // const [searchInput, setSearchInput] = useState("");
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   //Getting a list of the museums items categories
   async function getCategories() {
-    const {data} = await axios.get("https://collectionapi.metmuseum.org/public/collection/v1/departments");
-    console.log("data inside getCategories", data.departments);
+    const { data } = await axios.get(
+      "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+    );
+    // console.log("data inside getCategories", data.departments);
     setCategories(data.departments);
   }
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   // getting a list of the top items by the category selected
   async function getArtItemsIds(id) {
-    const {data} = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&departmentId=${id}&q=cat`);
-    console.log("data inside getArtItemsIds", data.objectIDs)
-    setArtItemsIds(data.objectIDs)
-    await getArtItems(artItemsIds)
+    const { data } = await axios.get(
+      `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&departmentId=${id}&q=cat`
+    );
+    console.log("data inside getArtItemsIds", data.objectIDs);
+    setArtItemsIds(data.objectIDs);
+    setCounter(0);
+    setArtItems([]);
   }
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  //getting each item by the id from recent function
-  // async function getArtItems(id=337068) {
   async function getArtItems() {
-    const {data} = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/337499`);
-    // const {data} = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`);
-    console.log("data inside getArtItems", data)
+    // console.log("counter before", counter);
+    for (let i = counter; i < counter + 30 && i < artItemsIds.length; i++) {
+      const { data } = await axios.get(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artItemsIds[i]}`
+      );
+      // console.log(data);
+      setArtItems((prev) => [...prev, data]);
+    }
+
+    setCounter((prev) => (prev = prev + 30));
   }
+  //---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  console.log("categories outside of return", categories)
-  return (<div className="art-list-header">
-    <h2>Art List</h2>
-    <button onClick={getCategories}>Get FU@&ING Categories!</button>
-    {categories.map((category, idx) => {
-      return (<div key={`${category.departmentId}${idx}`} id={category.departmentId}
-                   onClick={(e) => getArtItemsIds(e.target.id)}>{category.displayName}</div>)
-    })}
-    <br/>
-    {/*<button onClick={getArtItems}>Get Art</button>*/}
-  </div>)
+  // console.log("categories outside of return", categories);
+  return (
+    <div className="art-list-header">
+      {console.log("counter after", counter)}
+      {console.log("artItems", artItems)}
+      <h2>Art List</h2>
+      <button onClick={getCategories}>Get Categories!</button>
+      {/* SCROLLDOWN */}
+      {/* <select
+        value={"INITIAL"}
+        onChange={(e) => {
+          console.log("EEEE", e);
+          getArtItemsIds(e.target.id);
+        }}
+      >
+        {categories.map((category, idx) => {
+          return (
+            <option
+              key={`${category.departmentId}${idx}`}
+              id={category.departmentId}
+              value={category.departmentId}
+              // onClick={(e) => getArtItemsIds(e.target.id)}
+            >
+              {category.displayName}
+            </option>
+          );
+        })}
+      </select> */}
+      {/* BUTTONS */}
+      {categories.map((category, idx) => {
+        return (
+          <div
+            key={`${category.departmentId}${idx}`}
+            id={category.departmentId}
+            onClick={(e) => getArtItemsIds(e.target.id)}
+          >
+            {category.displayName}
+          </div>
+        );
+      })}
+      <br />
+      <div className="cardContainer">
+        {artItems.length > 0 &&
+          artItems.map((item) => {
+            const image = item.primaryImageSmall
+              ? item.primaryImageSmall
+              : item.primaryImage
+              ? item.primaryImage
+              : item.additionalImages[0];
+            return (
+              <div className="card" key={item.objectID} id={item.objectID}>
+                <h4>{item.title}</h4>
+                <p>
+                  <i>Artist: </i>
+                  {item.artistDisplayName ? item.artistDisplayName : "unknown"}
+                </p>
+                {item.medium && (
+                  <p>
+                    <i>Medium: </i> {item.medium}
+                  </p>
+                )}
+                {item.objectDate && (
+                  <p>
+                    <i>Year: </i> {item.objectDate}
+                  </p>
+                )}
+                {item.objectURL && (
+                  <a href={item.objectURL} target="_blank">
+                    Learn More
+                  </a>
+                )}
+                <img src={image} alt={item.title} />
+              </div>
+            );
+          })}
+      </div>
+      <button onClick={() => getArtItems(artItemsIds)}>Get Art Items!</button>
+    </div>
+  );
 }
-
 export default ArtItemsList;
